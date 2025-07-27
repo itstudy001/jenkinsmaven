ae8964e677844b2be3194c1f393070676fcc47c2
itstudy001 <itstudy@kakao.com>
2025-07-28 08:22:49 +0900

    jenkinsmaven

---
+++ src/main/java/com/adamsoft/jenkinsmaven/Calculator.java
@@ -0,0 +1,23 @@
+package com.adamsoft.jenkinsmaven;
+
+import org.springframework.stereotype.Service;
+@Service
+public class Calculator
+{
+    public int addition(int num1,int num2)
+    {
+        int R=num1+num2;
+        return R;
+    }
+    public int subtraction(int num1,int num2)
+    {
+        int Res=num1-num2;
+        return Res;
+    }
+    public int multiplication(int num1,int num2)
+    {
+        int Res=num1*num2;
+        return Res;
+    }
+}
+
